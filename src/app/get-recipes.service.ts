2764cb1f154c4c258d740bba480e8c3486857f4c
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-02-20 13:59:21 +0100

    Add getRecipes function with base Yummly API info.

--- src/app/get-recipes.service.ts
+++ src/app/get-recipes.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class GetRecipesService {
 
   constructor() { }
+
+
+
+  getRecipes(): void {
+    // Make api url here
+
+    // Yummly API information
+    const apiInfo: object = {
+        appId: 'eed9bde0',
+        appKey: '8ec48492633380399c709fec6e66a86f'
+    }
+    // TODO: use baseUrl to create custom searches with user input
+    // const baseUrl: string = http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters
+  }
+  
 }
+
+
