fb5eb5be5056cc4f84f9c426e4acbf443c59b349
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-02-21 14:46:48 +0100

    Add draft displaying of recipes.

--- src/app/search-recipes/search-recipes.component.ts
+++ src/app/search-recipes/search-recipes.component.ts
@@ -3,28 +3,29 @@ import { GetRecipesService } from '../get-recipes.service';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-search-recipes',
-  templateUrl: './search-recipes.component.html',
-  styleUrls: ['./search-recipes.component.scss']
+    selector: 'app-search-recipes',
+    templateUrl: './search-recipes.component.html',
+    styleUrls: ['./search-recipes.component.scss']
 })
 export class SearchRecipesComponent implements OnInit {
 
-  constructor(private GetRecipesService: GetRecipesService) { }
+    constructor(private GetRecipesService: GetRecipesService) { }
 
-  ngOnInit() {
-  }
+    public recipes: [];
 
-  getRecipes(searchTerm) {
-      let recipes: any;
+    ngOnInit() {
+    }
 
-      const observer = {
-          next: result => console.log(result),
-          error: err => console.log('An error has occured: ' + err)
-      }
-    
-      this.GetRecipesService.getRecipes(searchTerm)
-        .subscribe(observer);
+    getRecipes(searchTerm) {
+
+        const observer = {
+            next: result => console.log(this.recipes = result.matches),
+            error: err => console.log('An error has occured: ' + err)
+        }
+
+        this.GetRecipesService.getRecipes(searchTerm)
+            .subscribe(observer);
         
-  }
+    }
 
 }
