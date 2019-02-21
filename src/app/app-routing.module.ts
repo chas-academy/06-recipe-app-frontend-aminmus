49eb4f88efce71f4fbcc971f44e22a61b1228011
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-02-21 18:29:40 +0100

    Add default route, modify RecipesComponent template to still show recipe name if there is no recipe image.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // { path: '**', component: AppComponent},
+    { path:'', component: RecipesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
