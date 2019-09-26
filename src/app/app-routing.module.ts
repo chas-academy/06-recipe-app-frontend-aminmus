a63e8504d27d365ca31e42fce14c896fe304689f
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-26 22:00:38 +0200

    Move project into a newly generated Angular 8 app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
