71292c3003e23261b5041e24d133fd60e574f481
Amin Mushehid <amin.mushehid@gmail.com>
2020-03-28 21:02:42 +0100

    Add signup service

---
+++ src/app/signup.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { SIGNUP_MUTATION } from './graphql';
+import { Observable } from 'apollo-link';
+import { ApolloQueryResult } from 'apollo-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor(private apollo: Apollo) { }
+
+  public signup(name, email, password) {
+    return this.apollo.mutate({
+      mutation: SIGNUP_MUTATION,
+      variables: {
+        name,
+        email,
+        password,
+      },
+    }).subscribe(({ data }) => {
+      return data;
+    }, (error) => console.log('There was an error sending the query', error));
+  }
+}
