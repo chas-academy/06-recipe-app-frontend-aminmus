669506c0c38983916e8751918547234f9c856371
Amin Mushehid <amin.mushehid@gmail.com>
2020-05-29 19:09:22 +0200

    Add an authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { RecipeDetailsComponent } from './recipe-details/recipe-details.componen
 import { SearchRecipesComponent } from './search-recipes/search-recipes.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-    { path: 'search', component: SearchRecipesComponent },
-    { path: 'recipe/:uri', component: RecipeDetailsComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'login', component: LoginComponent }
+  { path: 'search', component: SearchRecipesComponent, canActivate: [AuthGuard] },
+  { path: 'recipe/:uri', component: RecipeDetailsComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor() {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      // if logged in return tu
+    return true;
+  }
+
+}
