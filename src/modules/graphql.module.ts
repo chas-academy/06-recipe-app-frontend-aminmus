0623eccdd8bfbf6b65f0a97fa3b5b0caa05859fd
Amin Mushehid <amin.mushehid@gmail.com>
2020-05-29 19:05:33 +0200

    Set authorization header and token on requests if it exists

--- src/modules/graphql.module.ts
+++ src/modules/graphql.module.ts
@@ -1,12 +1,26 @@
-import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS  } from 'apollo-angular';
+import { HttpLinkModule, HttpLink  } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 
 const uri = 'http://localhost:4000'; // <-- URL of GraphQL server
 export function createApollo(httpLink: HttpLink) {
+
+  const token = localStorage.getItem('token');
+  const authLink = setContext((_operation, _context) => {
+    if (token) {
+      return {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      };
+    }
+  });
+
   return {
-    link: httpLink.create({uri}),
+    link: ApolloLink.from([authLink, httpLink.create({ uri })]),
     cache: new InMemoryCache(),
   };
 }
@@ -21,4 +35,4 @@ export function createApollo(httpLink: HttpLink) {
     },
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule { }
