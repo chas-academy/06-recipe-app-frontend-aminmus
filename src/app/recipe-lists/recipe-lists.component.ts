11dbe4ae1231578466f4fb5a4957b4651f7654fb
Amin Mushehid <amin.mushehid@gmail.com>
2020-06-11 22:22:11 +0200

    Add ability to create a new recipe list in view

--- src/app/recipe-lists/recipe-lists.component.ts
+++ src/app/recipe-lists/recipe-lists.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { RecipeListService } from 'app/recipe-list.service';
+import { create } from 'domain';
 
 @Component({
   selector: 'app-recipe-lists',
@@ -8,14 +10,27 @@ import { RecipeListService } from 'app/recipe-list.service';
 })
 export class RecipeListsComponent implements OnInit {
 
-  constructor(private recipeListService: RecipeListService) { }
+  constructor(private recipeListService: RecipeListService, private formBuilder: FormBuilder) { }
 
-  private recipeLists;
+  public recipeLists;
+  public listForm: FormGroup;
 
   async ngOnInit() {
+    this.listForm = this.formBuilder.group({
+      listName: [''],
+    });
+
     const { data: { myRecipeLists: myRecipeLists } } = await this.recipeListService.getRecipeLists();
     this.recipeLists = myRecipeLists;
 
     console.log('all my lists ', this.recipeLists);
+  }
+
+  /** Create a new recipe list and update recipeLists variable */
+  async onSubmit() {
+    const { listName } = this.listForm.value;
+    const { data: { createRecipeList } } = await this.recipeListService.createRecipeList(localStorage.getItem('email'), listName);
+
+    this.recipeLists.push(createRecipeList);
   }
 }
